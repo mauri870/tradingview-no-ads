5c6f568aba48dfad07ab8a0724259884192e689c
Pedro Cabrita <pedro.cabrita@talkdesk.com>
2020-08-01 16:38:11 +0100

    Fix the selector to the Ads Toast

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 (function() {
   'use strict';
   const checkAd = setInterval(() => {
-    const adBox = document.getElementById('tv-toasts');
+    const adBox = document.querySelector("[class^='toast-positioning-wrapper-']")
+
     if (adBox) {
       adBox.remove();
       console.log('ad removed.');
